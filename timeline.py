aabf9410ee865f1539dbda933580e3ac4dd337f4
Hanyuesummer <Hanyuesummer@csu.fullerton.edu>
2021-10-18 00:39:28 -0700

    added server part for timeline.py

--- timeline.py
+++ timeline.py
@@ -1,1 +1,27 @@
 # add timeline python code here
+
+"""First hug API (local, command-line, and HTTP access)"""
+import hug
+
+
+@hug.cli()
+@hug.get(examples='userTimeline')
+@hug.local()
+
+# not sure what to put inside of the function 
+def user_timeline():
+    return {"success":True}
+
+def home_timeline():
+    return {"success":True}
+
+def public_timeline():
+    return {"success":True}
+
+if __name__ == '__main__':
+    user_timeline.interface.cli()
+    home_timeline.interface.cli()
+    public_timeline.interface.cli()
+
+#setting up a new port other than 8000
+hug.API(__name__).http.serve(port=8005)
