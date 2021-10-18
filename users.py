bb5d268975c61cb2a64821b933ca2a2b4cf24f08
Hanyuesummer <Hanyuesummer@csu.fullerton.edu>
2021-10-17 23:41:19 -0700

    added server part for user.py

--- users.py
+++ users.py
@@ -1,1 +1,15 @@
 # Add user python code here
+"""First hug API (local, command-line, and HTTP access)"""
+import hug
+
+
+@hug.cli()
+@hug.post(examples='CreateUsers')
+@hug.local()
+def create_user(username: hug.types.text, bio: hug.types.text, email: hug.types.text, password: hug.types.text, hug_timer=3):
+    
+    return {"success":True}
+
+
+if __name__ == '__main__':
+    create_user.interface.cli()
