ce68e9c5d84a0db66f326551b6c111384a8e8837
Sijan Rijal <47872289+sijanr@users.noreply.github.com>
2021-10-21 20:44:20 -0700

    Update posts.sql

--- posts.sql
+++ posts.sql
@@ -1,51 +1,63 @@
 PRAGMA foreign_keys = ON;
 
 CREATE TABLE IF NOT EXISTS posts(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT,
     post TEXT,
     timestamp TEXT,
-    repost TEXT,
-    PRIMARY KEY (username, timestamp)
+    repost TEXT
 );
 
 INSERT INTO posts (username, post, timestamp) VALUES(
     "reindeer",
     "I think I will buy the red car, or I will lease the blue one.",
-    "09-23-1923"
+    "1923-09-23 14:23:24"
 );
 
 INSERT INTO posts (username, post, timestamp) VALUES(
     "star-lord",
     "There was coal in his stocking and he was thrilled.",
-    "05-08-1934"
+    "1934-05-08 00:23:18"
 );
 
 INSERT INTO posts (username, post, timestamp) VALUES(
     "rye",
     "The busker hoped that the people passing by would throw money, but they threw tomatoes instead, so he exchanged his hat for a juicer.",
-    "08-11-1945"
+    "1945-08-11 08:45:54"
 );
 
 INSERT INTO posts (username, post, timestamp) VALUES(
     "thegreenmile",
     "There was coal in his stocking and he was thrilled.",
-    "07-28-2000"
+    "2000-07-28 02:09:32"
 );
 
 INSERT INTO posts (username, post, timestamp) VALUES(
     "reindeer",
     "We need to rent a room for our party.",
-    "09-24-1923"
+    "1923-09-24 09:27:43"
 );
 
 INSERT INTO posts (username, post, timestamp) VALUES(
     "thegreenmile",
     "She thought there'd be sufficient time if she hid her watch.",
-    "09-23-1923"
+    "1923-09-23 10:10:26"
 );
 
 INSERT INTO posts (username, post, timestamp) VALUES(
     "rye",
     "The bug was having an excellent day until he hit the windshield.",
-    "01-23-1983"
+    "1983-01-23 18:05:13"
+);
+
+INSERT INTO posts (username, post, timestamp) VALUES(
+    "petrichor",
+    "Homesickness became contagious in the young campers' cabin.",
+    "1948-04-13 23:35:45"
+);
+
+INSERT INTO posts (username, post, timestamp) VALUES(
+    "thedeparted",
+    "A kangaroo is really just a rabbit on steroids.",
+    "1998-06-07 22:30:43"
 );
