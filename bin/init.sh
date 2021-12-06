2d5ec73d08f97dbebc0264d2dcae2ba20d7a8bd6
Hanyue Zheng <summer199303@gmail.com>
2021-12-05 16:23:29 -0800

    Make bin/init.sh work even if db files don't exist

--- bin/init.sh
+++ bin/init.sh
@@ -1,4 +1,4 @@
-rm ./var/users.db
-rm ./var/posts.db
+touch ./var/users.db && rm ./var/users.db
+touch ./var/posts.db && rm ./var/posts.db
 sqlite3 ./var/users.db < users.sql
 sqlite3 ./var/posts.db < posts.sql
