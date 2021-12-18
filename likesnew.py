d7daba2599b62b82b673addf31377125500d8c6b
Yash <yash_b@csu.fullerton.edu>
2021-12-17 19:42:42 -0800

    adding likes

---
+++ likesnew.py
@@ -0,0 +1,54 @@
+
+import configparser
+import logging.config
+from typing import NamedTuple
+import hug
+import redis
+import json
+
+
+# Load configuration
+#
+config = configparser.ConfigParser()
+config.read("./etc/api.ini")
+logging.config.fileConfig(config["logging"]["config"], disable_existing_loggers=False)
+r = redis.Redis(host='localhost', port=6379, db=0,decode_responses=True)
+
+
+#Get Popular posts
+@hug.get("/likes/popular") 
+def popularLikes(response):
+    topPost = r.zrange("postlikes",0,4, desc=True,withscores=True)
+    posts =[]
+    for post in topPost:
+        posts.append({"postid": post[0].decode(), "likes": post[1] })
+    # print(topPost, "=============")
+    return {"data": posts}
+
+#Like a post
+@hug.post("/like",status=hug.falcon.HTTP_201) 
+def likepost(response, username, postid):
+    exists = r.sismember("user"+username, postid)
+    if exists: #its True
+        return {"message": "User has already liked the post"}
+    else:
+        r.sadd("user"+username, postid)
+        rank=r.zrank("postlikes", postid)
+        if(rank!=None):
+            rank = int(rank) + 1
+        else:
+            rank = 1
+        r.zadd("postlikes", {postid: rank} )
+
+    return {"message": "Post liked successfully"}
+
+#Get posts liked by a user
+@hug.get("/likes/{username}") 
+def popularLikes(response,username):
+   
+    posts = r.smembers("user"+username)
+    postsnew = []
+    for post in posts:
+        # print(post,"=======")
+        postsnew.append(post)
+    return {"data": postsnew}
